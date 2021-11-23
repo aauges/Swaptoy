15fd3203e350258cca971fe0ccff8fe8a0d22fb6
Olle Thestrup <thestrup@unlv.nevada.edu>
2021-11-23 17:48:07 +0100

    btn

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def kitchensink
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # devise_for :users
   root to: 'pages#home'
-
+  get '/kitchensink', to: 'pages#kitchensink'
   resources :toys, only: [:index, :create, :show, :new, :edit, :destroy]
 end
