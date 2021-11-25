f723a0a177e792019ea05d8253312cdab43a56ac
Filip Zdravkovic <fzdravkovic1@gmail.com>
2021-11-25 11:14:08 +0100

    update2

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_11_24_104137) do
-=======
 ActiveRecord::Schema.define(version: 2021_11_24_165139) do
 
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,9 +45,6 @@ ActiveRecord::Schema.define(version: 2021_11_24_165139) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["monthly_subscription_id"], name: "index_bookings_on_monthly_subscription_id"
     t.index ["toy_id"], name: "index_bookings_on_toy_id"
-<<<<<<< HEAD
-    t.index ["user_id"], name: "index_bookings_on_user_id"
-=======
   end
 
   create_table "monthly_subscriptions", force: :cascade do |t|
@@ -64,7 +57,6 @@ ActiveRecord::Schema.define(version: 2021_11_24_165139) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_monthly_subscriptions_on_user_id"
->>>>>>> master
   end
 
   create_table "toys", force: :cascade do |t|
@@ -91,14 +83,8 @@ ActiveRecord::Schema.define(version: 2021_11_24_165139) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bookings", "toys"
-  add_foreign_key "bookings", "users"
-=======
   add_foreign_key "bookings", "monthly_subscriptions"
   add_foreign_key "bookings", "toys"
   add_foreign_key "monthly_subscriptions", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
->>>>>>> master
 end
