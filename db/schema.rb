7da3f1bb0ca0eb6a3b329349639660613caa8c09
albert auge <aaugeserrano@gmail.com>
2021-11-25 11:20:11 +0100

    update

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2021_11_24_165139) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_165139) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "bookings", force: :cascade do |t|
     t.boolean "return_status"
@@ -83,8 +82,8 @@ ActiveRecord::Schema.define(version: 2021_11_24_165139) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "monthly_subscriptions"
   add_foreign_key "bookings", "toys"
   add_foreign_key "monthly_subscriptions", "users"
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
