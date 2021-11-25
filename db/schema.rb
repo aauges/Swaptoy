03c5143dfba34f8ee1642a43173d492edecfa096
Filip Zdravkovic <fzdravkovic1@gmail.com>
2021-11-25 13:58:52 +0100

    views

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2021_11_24_165139) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["monthly_subscription_id"], name: "index_bookings_on_monthly_subscription_id"
     t.index ["toy_id"], name: "index_bookings_on_toy_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "monthly_subscriptions", force: :cascade do |t|
@@ -83,8 +82,8 @@ ActiveRecord::Schema.define(version: 2021_11_24_165139) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "monthly_subscriptions"
   add_foreign_key "bookings", "toys"
   add_foreign_key "monthly_subscriptions", "users"
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
