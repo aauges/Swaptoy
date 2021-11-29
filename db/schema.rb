de0801fd511815658fa8fd02ba260ad878fc1547
albert auge <aaugeserrano@gmail.com>
2021-11-29 15:57:18 +0100

    fake payment functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_084312) do
+ActiveRecord::Schema.define(version: 2021_11_29_130617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_11_29_084312) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price_cents", default: 0, null: false
     t.index ["user_id"], name: "index_monthly_subscriptions_on_user_id"
   end
 
@@ -89,6 +90,10 @@ ActiveRecord::Schema.define(version: 2021_11_29_084312) do
     t.boolean "sub_model"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_id"
+    t.string "stripe_subscription_id"
+    t.string "checkout_session_id"
+    t.string "stripe_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
